1c7232891bef5e65a3f95e3b80c8e1abba78d5e7
bansi <bansidobariya2510@gmail.com>
2025-04-09 12:08:45 +0100

    Add new feature: quiz questions fetched from backend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./Header";
+import "./index.css";
+import Main from "./Main";
+import { useEffect, useReducer } from "react";
+
+const initialState = {
+  questions: [],
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "datarec":
+      return {
+        ...state,
+        questions: action.payload,
+        status: "ready",
+      };
+    case "datafailed":
+      return {
+        ...state,
+        status: "error",
+      };
+    default:
+      throw new Error("Unknown action type");
+  }
+}
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  useEffect(() => {
+    fetch("http://localhost:8000/questions")
+      .then((response) => response.json())
+      .then((data) => dispatch({ type: "datarec", payload: data }))
+
+      .catch((error) => dispatch({ type: "datafailed" }));
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Main className="main"></Main>
     </div>
   );
 }
